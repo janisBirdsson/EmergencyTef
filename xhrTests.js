e50dd95242a30a3aa3a017e1e4bee190a9c139a2
HurrenCodes <142058088+HurrenCodes@users.noreply.github.com>
2024-02-08 16:56:33 +0000

    Update xhrTests.js

--- xhrTests.js
+++ xhrTests.js
@@ -6,21 +6,27 @@ function requestHttp(url, successCallback, errorCallback, resquestHeaders = [], 
     xhr = new ActiveXObject("Microsoft.XMLHTTP");
   }
   
+  // xhr.onreadystatechange = () => {
+  //   try{
+  //     if(xhr.readyState == 4){
+  //       var status = xhr.status;
+  //       if(status >= 200 && status < 400 && xhr.HEADERS_RECEIVED){
+  //         successCallback(xhr);
+  //       }else{
+  //         console.log("Problem connecting to " + url + ". Status: " + status);
+  //         errorCallback(xhr);
+  //       }
+  //     }
+  //   }catch(e){
+  //     console.log(e);
+  //   }
+  // };
+  xhr.onload = () => {
+    console.log("onload " + xhr.getResponseHeader('Location'));
+  }
   xhr.onreadystatechange = () => {
-    try{
-      if(xhr.readyState == 4){
-        var status = xhr.status;
-        if(status >= 200 && status < 400 && xhr.HEADERS_RECEIVED){
-          successCallback(xhr);
-        }else{
-          console.log("Problem connecting to " + url + ". Status: " + status);
-          errorCallback(xhr);
-        }
-      }
-    }catch(e){
-      console.log(e);
-    }
-  };
+    console.log("onreadystatechange " + xhr.getResponseHeader('Location'));
+  }
   xhr.ontimeout = () => {
     console.log("Connection to " + url + "Timed out.");
     errorCallback(xhr);
@@ -44,16 +50,16 @@ function requestHttp(url, successCallback, errorCallback, resquestHeaders = [], 
   xhr.send();
 }
 
-console.log("Test2!\n");
+console.log("Test!\n");
 const target = "https://" + window.location.hostname.substring(0, 29);
-console.log("Host: \"" + target + "\"\n");
+// console.log("Host: \"" + target + "\"\n");
 
-requestHttp(
-  target + "/community/sylvanrah",
-  (xhr) => {console.log("success"); console.log(xhr)},
-  (xhr) => {console.log("fail")},
-  [["Cookie", "SESS3d506d9bc855ddcd3287f9913e63767c=5nthk9qrkek76ftrnu48jq5g42; has_js=1; SESS903465f0adcfe01479847cb3d1bb9c52=3k7gr84qkdlpa1pvqatpveq262"]]
-);
+// requestHttp(
+//   target + "/community/sylvanrah",
+//   (xhr) => {console.log("success"); console.log(xhr)},
+//   (xhr) => {console.log("fail")},
+//   [["Cookie", "SESS3d506d9bc855ddcd3287f9913e63767c=5nthk9qrkek76ftrnu48jq5g42; has_js=1; SESS903465f0adcfe01479847cb3d1bb9c52=3k7gr84qkdlpa1pvqatpveq262"]]
+// );
 
 requestHttp(
   target + "/machine/playerpage.php?symbol=2Kdf4",
@@ -61,6 +67,8 @@ requestHttp(
   (xhr) => {console.log("fail")},
   [["Cookie", "SESS3d506d9bc855ddcd3287f9913e63767c=5nthk9qrkek76ftrnu48jq5g42; has_js=1; SESS903465f0adcfe01479847cb3d1bb9c52=3k7gr84qkdlpa1pvqatpveq262"]]
 );
+
+// either cookie except has_js works
 
 // [["Content-Security-Policy", "upgrade-insecure-requests"],
 // ["Cookie", "SESS3d506d9bc855ddcd3287f9913e63767c=5nthk9qrkek76ftrnu48jq5g42; has_js=1; SESS903465f0adcfe01479847cb3d1bb9c52=3k7gr84qkdlpa1pvqatpveq262"]]
