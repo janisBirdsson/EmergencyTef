085a810d755a8c1bc6044ba10ae862074afccaae
HurrenCodes <142058088+HurrenCodes@users.noreply.github.com>
2024-02-08 19:48:53 +0000

    Update xhrTests.js

--- xhrTests.js
+++ xhrTests.js
@@ -66,7 +66,64 @@ function listCookies() {
   return aString;
 }
 
+function getPlayers(successCallback) {
+  const url = "/machine/playerstatus.php?action=get";
+  let xhr = null;
+  if(window.XMLHttpRequest){
+    xhr = new XMLHttpRequest();
+  }else{
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  
+  xhr.onreadystatechange = () => {
+    try{
+      if(xhr.readyState == 4){
+        var status = xhr.status;
+        if(status >= 200 && status < 400 && xhr.HEADERS_RECEIVED){
+          successCallback(xhr);
+        }else{
+          console.log("Problem connecting to " + url + ". Status: " + status);
+        }
+      }
+    }catch(e){
+      console.log(e);
+    }
+  };
+  xhr.ontimeout = () => {
+    console.log("Connection to " + url + "Timed out.");
+  };
+  xhr.open(method, url, true);
+  // xhr.setRequestHeader("Content-Security-Policy", "upgrade-insecure-requests");
+  // xhr.setRequestHeader("Upgrade-Insecure-Requests", "1");
+  console.log(resquestHeaders);
+  resquestHeaders.forEach(pair => {
+    console.log(pair[0] + ":" + pair[1]);
+    xhr.setRequestHeader(pair[0], pair[1]);
+  });
+  if(noCache){
+    xhr.setRequestHeader(
+      "Cache-Control", "no-cache, no-store, max-age=0, must-revalidate"
+    );
+    xhr.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT");
+    xhr.setRequestHeader("Pragma", "no-cache");
+  }
+  //xhr.timeout = 2 * 1000;
+  xhr.send();
+}
+
 // either cookie except has_js works
 // TODO somethig about xhr.accept("text") vs "document"
+
+function updateMap(xhr) {
+  let playerListElem = document.createElement("html");
+  playerListElem.innerHTML = xhr.responseText;
+  const players = playerListElem.querySelectorAll("player");
+  const picto = players[0].getAttribute("pictogram");
+  console.log(picto);
+}
+
+getPlayers((xhr) => {
+  updateMap(xhr);
+})
 
 // [["Content-Security-Policy", "upgrade-insecure-requests"],
