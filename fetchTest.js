df94bcb3436d3348810ef3b7c0b3cbea9e798f89
HurrenCodes <142058088+HurrenCodes@users.noreply.github.com>
2024-02-09 16:37:52 +0000

    Update fetchTest.js

--- fetchTest.js
+++ fetchTest.js
@@ -3,7 +3,9 @@ function test(url, redi) {
   fetch(url, { method: 'POST', redirect: redi})
     .then(response => {
       // HTTP 301 response
-      console.log("response.url =", response.url);
+      console.log("response.url =", response.url + "\n");
+      console.log("headsers: " + response.headers + "\n");
+      response.headers.forEach(function(val, key) { console.log(key + ' -> ' + val); });
     })
     .catch(function(err) {
       console.info(err + " url: " + url);
