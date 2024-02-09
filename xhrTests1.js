b4e300ed82a6e55290cb9f7afcaf58370df2cd72
HurrenCodes <142058088+HurrenCodes@users.noreply.github.com>
2024-02-09 00:44:42 +0000

    Update xhrTests1.js

--- xhrTests1.js
+++ xhrTests1.js
@@ -246,7 +246,7 @@ function updateMap(xhr) {
   );
 }
 
-// window.location = "http://www.endlessforest.org/community/node/114789"; 
+window.location = "http://www.endlessforest.org/community/node/114789"; 
 
 getPlayers((xhr) => {
   updateMap(xhr);
